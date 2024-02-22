1fbe07af90dd555b039f5e5b788cee8acaa9b540
Shrutimishraa <shruti19122001@gmail.com>
2024-02-22 11:44:57 +0530

    .....

---
+++ quest4.py
@@ -0,0 +1,10 @@
+"""Ques:4
+Write a   program to accept n numbers from the user ,
+ store these numbers into an array and 
+ sort the numbers of an array using a function."""
+print("Question:3") 
+from array import*      
+n=int(input('Enter the number : '))
+arr
+for i 
+
